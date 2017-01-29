704c1e22480d629da45eb0542da25e75c643d366
Stefan Horne <Stefan Horne>
2017-01-29 18:42:32 +0000

    Added custom vendor npc which spawns after world loads for the first time

---
+++ NPCs/TownNPC.cs
@@ -0,0 +1,154 @@
+﻿using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RandomCustomItems.NPCs
+{
+    class TownNPC : ModNPC
+    {
+        public override bool Autoload(ref string name, ref string texture, ref string[] altTextures)
+        {
+            name = "Town NPC";
+            altTextures = new string[] { "RandomCustomItems/NPCs/TownNPC_Alt_1" };
+            return mod.Properties.Autoload;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.name = "Town NPC";
+            npc.townNPC = true;
+            npc.friendly = true;
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 7;
+            npc.damage = 10;
+            npc.defense = 15;
+            npc.lifeMax = 250;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.knockBackResist = 0.5f;
+            Main.npcFrameCount[npc.type] = 25;
+            NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+            NPCID.Sets.AttackFrameCount[npc.type] = 4;
+            NPCID.Sets.DangerDetectRange[npc.type] = 700;
+            NPCID.Sets.AttackType[npc.type] = 0;
+            NPCID.Sets.AttackTime[npc.type] = 90;
+            NPCID.Sets.AttackAverageChance[npc.type] = 30;
+            NPCID.Sets.HatOffsetY[npc.type] = 4;
+            NPCID.Sets.ExtraTextureCount[npc.type] = 1;
+            animationType = NPCID.Guide;
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            int num = npc.life > 0 ? 1 : 5;
+            for (int k = 0; k < num; k++)
+            {
+                Dust.NewDust(npc.position, npc.width, npc.height, mod.DustType("Sparkle"));
+            }
+        }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs, int money)
+        {
+            /*
+            // this iterates through every player
+            for (int k = 0; k < 255; k++)
+            {
+                Player player = Main.player[k];
+                if (player.active)
+                {
+                    
+                    // iterates through all items in player inventory
+                    for (int j = 0; j < player.inventory.Length; j++)
+                    {
+                        if (player.inventory[j].type == mod.ItemType("ExampleItem") || player.inventory[j].type == mod.ItemType("ExampleBlock"))
+                        {
+                            return true;
+                        }
+                    }
+                    
+                }
+            }*/
+            return true;
+        }
+
+        public override bool CheckConditions(int left, int right, int top, int bottom)
+        {
+            return true;
+        }
+
+        public override string TownNPCName()
+        {
+            switch (WorldGen.genRand.Next(4))
+            {
+                case 0:
+                    return "Someone";
+                case 1:
+                    return "Somebody";
+                case 2:
+                    return "Blocky";
+                default:
+                    return "Colorless";
+            }
+        }
+
+        public override string GetChat()
+        {
+            switch (Main.rand.Next(3))
+            {
+                case 0:
+                    return "Please, examine my wares.";
+                case 1:
+                    return "Snoop around.";
+                default:
+                    return "I have the perfect cure for your sunny disposition my fine friend.";
+            }
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = Lang.inter[28];
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref bool shop)
+        {
+            if (firstButton)
+            {
+                shop = true;
+            }
+        }
+
+        public override void SetupShop(Chest shop, ref int nextSlot)
+        {
+            shop.item[nextSlot].SetDefaults(mod.ItemType("DickStaff"));
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults(mod.ItemType("SickSwordOfSuck"));
+            nextSlot++;
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 20;
+            knockback = 4f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 30;
+            randExtraCooldown = 30;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = ProjectileID.FireArrow;
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 12f;
+            randomOffset = 2f;
+        }
+    }
+}
---
+++ RandomCustomWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.World.Generation;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using System.Linq;
+using Terraria.ModLoader.IO;
+
+namespace RandomCustomItems
+{
+    class RandomCustomWorld : ModWorld
+    {
+        public override void PostWorldGen()
+        {
+            int num = NPC.NewNPC((Main.spawnTileX + 5) * 16, Main.spawnTileY * 16, mod.NPCType("Town NPC"), 0, 0f, 0f, 0f, 0f, 255);
+            Main.npc[num].homeTileX = Main.spawnTileX + 5;
+            Main.npc[num].homeTileY = Main.spawnTileY;
+            Main.npc[num].direction = 1;
+            Main.npc[num].homeless = true;
+        }
+    }
+}
